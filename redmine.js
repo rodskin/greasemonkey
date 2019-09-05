843e53965d55db5d1bcd20788688dbebae1aa986
Rodskin <rodskin@users.noreply.github.com>
2019-09-05 14:48:37 +0200

    added redmine.js
    code to add buttons: 
    a separator 
    @TODO
    @DONE
    @INFO
    @CANCELED

---
+++ redmine.js
@@ -0,0 +1,96 @@
+// ==UserScript==
+// @name     Redmine add buttons
+// @version  1
+// @grant    none
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js 
+// ==/UserScript==
+
+
+// SEPARATOR
+function mySeparator() {
+  var issues_text = jQuery('#issue_notes').val();
+  issues_text += "\r\n\r\n" + '----' + "\r\n\r\n";
+  jQuery('#issue_notes').val(issues_text);
+  return false;
+}
+var elem = document.createElement("input"); 
+elem.value = '----';
+elem.type = 'button';
+elem.style.background = 'white';
+elem.style.marginRight = '4px';
+
+elem.addEventListener('click', mySeparator, false);
+
+jQuery(".jstElements").append(elem);
+
+
+// TODO
+function myTodo() {
+  var issues_text = jQuery('#issue_notes').val();
+  issues_text += "\r\n" + '%{background:orange}@TODO% : ';
+  jQuery('#issue_notes').val(issues_text);
+  return false;
+}
+var elem = document.createElement("input"); 
+elem.value = '@TODO';
+elem.type = 'button';
+elem.style.background = 'orange';
+elem.style.marginRight = '4px';
+
+elem.addEventListener('click', myTodo, false);
+
+jQuery(".jstElements").append(elem);
+
+
+// DONE
+function myDone() {
+  var issues_text = jQuery('#issue_notes').val();
+  issues_text += "\r\n" + '%{background:lightgreen}@DONE% : ';
+  jQuery('#issue_notes').val(issues_text);
+  return false;
+}
+var elem = document.createElement("input"); 
+elem.value = '@DONE';
+elem.type = 'button';
+elem.style.background = 'lightgreen';
+elem.style.marginRight = '4px';
+
+elem.addEventListener('click', myDone, false);
+
+jQuery(".jstElements").append(elem);
+
+
+// INFO
+function myInfo() {
+  var issues_text = jQuery('#issue_notes').val();
+  issues_text += "\r\n" + '%{background:yellow}@INFO% : ';
+  jQuery('#issue_notes').val(issues_text);
+  return false;
+}
+var elem = document.createElement("input"); 
+elem.value = '@INFO';
+elem.type = 'button';
+elem.style.background = 'yellow';
+elem.style.marginRight = '4px';
+
+elem.addEventListener('click', myInfo, false);
+
+jQuery(".jstElements").append(elem);
+
+
+// CANCELED
+function myCancel() {
+  var issues_text = jQuery('#issue_notes').val();
+  issues_text += "\r\n" + '%{background:red; color:white}@CANCELED% : ';
+  jQuery('#issue_notes').val(issues_text);
+  return false;
+}
+var elem = document.createElement("input"); 
+elem.value = '@CANCELED';
+elem.type = 'button';
+elem.style.background = 'red';
+elem.style.color = 'white';
+
+elem.addEventListener('click', myCancel, false);
+
+jQuery(".jstElements").append(elem);
